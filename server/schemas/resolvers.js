7ea9e4de962b343ef907e0f722a0b5a5b873e171
Michelle <mchristine.stone91@gmail.com>
2022-03-16 01:55:47 -0400

    Added user and thought typeDefs and provided query information for all and single users and thoughts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,37 @@ const { User, Thought } = require("../models");
 // Returning thought data in descending order - .sort().
 const resolvers = {
   Query: {
-    thoughts: async () => {
-      return Thought.find().sort({ createdAt: -1 });
+    // Pass parent as a placeholder parameter - need something in first spot.
+    // User ternary operator to check if username exists.
+    // If exists: set params to object with username key set to that value.
+    // If it doesn't, return an empty object.
+    // Pass object to .find() which looks up by specific username.
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+
+    // Destructure _id and place it into our .findOne() to look up single thought by its id.
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+
+    // get all users
+    users: async () => {
+      return (
+        User.find()
+          // Leave out __v property and user's password information.
+          .select("-__v -password")
+          .populate("friends")
+          .populate("thoughts")
+      );
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
     },
   },
 };
